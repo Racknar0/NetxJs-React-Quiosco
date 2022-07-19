63c2b07cda768b721e78b3c1e54bc397008281f6
Camilo Torres <racknarow1@gmail.com>
2022-07-19 00:15:49 -0500

    add consultas

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
+import { PrismaClient } from 'prisma/prisma-client';
 
-export default function Home() {
-  return (
-    <h1>Nextjs</h1>
-  )
+export default function Home({ categorias }) {
+
+    console.log(categorias); 
+    return <h1>Nextjs</h1>;
 }
+
+export const getServerSideProps = async () => {
+    const prisma = new PrismaClient();
+
+    const categorias = await prisma.categoria.findMany();
+
+    return {
+        props: {
+            categorias,
+        },
+    };
+};
