2a4481b131197648238af0ead3a9dda9a908db06
Camilo Torres <racknarow1@gmail.com>
2022-07-19 10:41:48 -0500

    add logo

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Sidebar from '../components/Sidebar';
 
 export default function Layout({ children, pagina }) {
     return (
@@ -9,11 +10,11 @@ export default function Layout({ children, pagina }) {
         </Head>
 
         <div className='md:flex'>
-            <aside className='md:w-4/12'>
-                <h1>Sidebar aqui</h1>
+            <aside className='md:w-4/12 xl:w-1/4 2xl:1/5'>
+                <Sidebar />
             </aside>
 
-            <main className='md:w-8/12'>
+            <main className='md:w-8/12 xl:w-3/4 2xl:4/5 h-screen overflow-y-scroll'>
                 {children}
             </main>
         </div>
